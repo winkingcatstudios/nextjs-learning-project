d6237d4994e030a5d8d1a9fd3824938fc99feb82
winkingcat <dankercher@gmail.com>
2023-06-07 13:54:14 -0400

    add pages skeleton

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,19 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Hero from "public/hero.png";
+import Button from "@/components/button/Button";
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Dice for your cats</h1>
+        <p className={styles.desc}>Let em roll</p>
+        <Button url="/portfolio" text="See our dice">See our dice</Button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="" className={styles.img} />
+      </div>
     </div>
-  )
+  );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
 import styles from "./footer.module.css"
+import Image from 'next/image'
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>@2023 Winking Cat Studio. All rights reserved.</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Dice Cats"/>
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Dice Cats"/>
+        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Dice Cats"/>
+        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Dice Cats"/>
+      </div>
     </div>
   )
 }
